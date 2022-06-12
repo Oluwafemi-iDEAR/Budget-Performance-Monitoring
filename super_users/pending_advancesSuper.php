77f36a54973aaea8ee35056462345a0efa2fd990
Oluwafemi-iDEAR <ifesanmioluwafemi@gmail.com>
2022-06-12 14:54:57 +0100

    initial

---
+++ super_users/pending_advancesSuper.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Activity Log</title>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../thirdparties/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link href="../images/Jesus-Light-of-the-World.png" rel="icon" type="image/png">
+
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="topnav wow fadeIn" data-wow-duration="9s">
+
+      <!-- Centered link -->
+      <div class="topnav-centered">
+        <a href="#home" class="active"><img src="../images/Jesus-Light-of-the-World.png" height="50" width="100" class="d-none d-md-block"></a>
+      </div>
+
+      <!-- Left-aligned links (default) -->
+      <a href="#"><strong>YDD</strong></a>
+      <!-- <a href="#contact">Contact</a> -->
+
+      <!-- Right-aligned links -->
+      <div class="topnav-right">
+        <!-- <a href="#search">Search</a> -->
+        <a href="#about"><strong>2021</strong></a>
+      </div>
+
+    </div>
+
+  </div>
+  <div class="space-200"></div>
+  <div class="main-panel">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 grid-margin stretch-card mb-3">
+          <div class="card" style=" border-radius: 21px 0 0 21px;">
+            <div class="profile-card">
+
+              <div class="profile-header">
+
+                <div class="cover-image">
+                  <img src="../images/unnamed.png" class="img img-fluid">
+                </div>
+                <div class="user-image">
+                  <img src="../images/avatar.png" class="img ">
+                </div>
+              </div>
+
+              <div class="profile-content">
+                <div class="profile-name">Oluwafemi Ifesanmi</div>
+                <div class="profile-designation">User<a href="../profile.html" class="about-item-edit">Edit</a></div>
+                <ul class="profile-info-list">
+                  <a href="index.html" class="profile-info-list-item active"><i class="fa fa-dashboard"></i>Dashboard</a>
+
+                  <li class="">
+                    <button style="margin: 10px 0; width: 220px; padding:15px;" class="btn button button2 btn-toggle align-items-center collapsed profile-info-list-item" data-bs-toggle="collapse" data-bs-target="#wr-collapse" aria-expanded="false">
+                      WECA Reports
+                    </button>
+                    <div class="collapse" id="wr-collapse">
+                      <hr class="divider">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="wecareports_bt.html" class="link-dark profile-info-list-item rounded">Budget Template</a></li>
+                        <li><a href="wecareports_ctd.html" class="link-dark profile-info-list-item rounded">Cost to Date</a></li>
+                        <li><a href="wecareports_ur.html" class="link-dark profile-info-list-item rounded">Utilization Reports</a></li>
+                        <li><a href="wecareports_rar.html" class="link-dark profile-info-list-item rounded">Revenue Actualisation Report</a></li>
+                        <li><a href="wecareports_ieur.html" class="link-dark profile-info-list-item rounded">Income & Expenditure Utilization Report</a></li>
+                      </ul>
+                      <hr class="divider">
+                    </div>
+                  </li>
+                  <li class="">
+                    <button style="margin: 10px 0; width: 220px; padding:15px;" class="btn button button2 btn-toggle align-items-center collapsed profile-info-list-item" data-bs-toggle="collapse" data-bs-target="#bs-collapse" aria-expanded="false">
+                      Budget Submission
+                    </button>
+                    <div class="collapse" id="bs-collapse">
+                      <hr class="divider">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Family Development</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Youth Development</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">DS Office</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Corporate Planning and Investment</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Treasury</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Infrastructure</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Adminstration</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Francophone</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Missions</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Worship</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Anthony </a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Publishing</a></li>
+                      </ul>
+                      <hr class="divider">
+                    </div>
+                  </li>
+                  <li class="">
+                    <button style="margin: 10px 0; width: 220px; padding:15px;" class="btn button button2 btn-toggle align-items-center collapsed profile-info-list-item" data-bs-toggle="collapse" data-bs-target="#ur-collapse" aria-expanded="false">
+                      Utilization Reports
+                    </button>
+                    <div class="collapse" id="ur-collapse">
+                      <hr class="divider">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Family Development</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Youth Development</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">DS Office</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Corporate Planning and Investment</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Treasury</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Infrastructure</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Adminstration</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Francophone</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Missions</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Worship</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Anthony </a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Publishing</a></li>
+                      </ul>
+                      <hr class="divider">
+                    </div>
+                  </li>
+                  <li class="">
+                    <button style="margin: 10px 0; width: 220px; padding:15px;" class="btn button button2 btn-toggle align-items-center collapsed profile-info-list-item" data-bs-toggle="collapse" data-bs-target="#budget-collapse" aria-expanded="false">
+                      Create Budget
+                    </button>
+                    <div class="collapse" id="budget-collapse">
+                      <hr class="divider">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Treasury</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Anthony Operations</a></li>
+                      </ul>
+                      <hr class="divider">
+                    </div>
+                  </li>
+                  <li class="">
+                    <button style="margin: 10px 0; width: 220px; padding:15px;" class="btn button button2 btn-toggle align-items-center collapsed profile-info-list-item" data-bs-toggle="collapse" data-bs-target="#ta-collapse" aria-expanded="false">
+                      Transactions Approval
+                    </button>
+                    <div class="collapse" id="ta-collapse">
+                      <hr class="divider">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="pending_status.html" class="link-dark profile-info-list-item rounded">Pending</a></li>
+                        <li><a href="approved_status.html" class="link-dark profile-info-list-item rounded">Approved</a></li>
+                      </ul>
+                      <hr class="divider">
+                    </div>
+                  </li>
+                  <li class="">
+                    <button style="margin: 10px 0; width: 220px; padding:15px;" class="btn button button2 btn-toggle align-items-center collapsed profile-info-list-item" data-bs-toggle="collapse" data-bs-target="#ta-collapse" aria-expanded="false">
+                      Retirement Approval
+                    </button>
+                    <div class="collapse" id="ta-collapse">
+                      <hr class="divider">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="pending_retirement.html" class="link-dark profile-info-list-item rounded">Pending</a></li>
+                        <li><a href="approved_retirement.html" class="link-dark profile-info-list-item rounded">Approved</a></li>
+                      </ul>
+                      <hr class="divider">
+                    </div>
+                  </li>
+                  <a href="#" class="link-dark profile-info-list-item rounded">Advance Approvals</a>
+                  <li class="">
+                    <button style="margin: 10px 0; width: 220px; padding:15px;" class="btn button button2 btn-toggle align-items-center collapsed profile-info-list-item" data-bs-toggle="collapse" data-bs-target="#aa-collapse" aria-expanded="false">
+                      Admin Access
+                    </button>
+                    <div class="collapse" id="aa-collapse">
+                      <hr class="divider">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Users</a></li>
+                        <li><a href="#" class="link-dark profile-info-list-item rounded">Periods</a></li>
+                      </ul>
+                      <hr class="divider">
+                    </div>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-9 grid-margin stretch-card">
+
+          <div class="card">
+            <div class="col p-4">
+              <div class="card myForm">
+                <h5 class="card-header font-weight-light"><i class="fa fa-file" aria-hidden="true"></i> View Details</h5>
+                <div class="card-body">
+                  <div class="container">
+                    <div class="row clearfix">
+                      <div class="table-responsive">
+                        <table id="example" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                          <thead>
+                            <tr>
+                              <th> S/N</th>
+                              <th> Request Date</th>
+                              <th> Code</th>
+                              <th> Above the Line</th>
+                              <th> Budget Line</th>
+                              <th> Amount</th>
+                              <th> Status</th>
+                              <!-- <th> Outstanding</th> -->
+                              <th> Action</th>
+
+                            </tr>
+                          </thead>
+                          <tbody>
+
+                            <?php view_UnritiredSU(); ?>
+
+                          </tbody>
+                          <tfoot>
+                            <tr>
+                              <td>
+                                <strong>Total</strong>
+                              </td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td>₦<?php echo number_format($_SESSION['make_retirementSum']); ?></td>
+
+                              <td></td>
+
+                              <td></td>
+                              <!-- <td>₦1,230,059.80</td> -->
+
+                          </tfoot>
+                        </table>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- content-wrapper ends -->
+  <!-- main-panel ends -->
+  <script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/selectFilter.min.js"></script>
+  <script src="../thirdparties/bootstrap/bootstrap.min.js"></script>
+</body>
+
+</html>
